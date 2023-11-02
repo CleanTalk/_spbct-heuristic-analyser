d25a7743da38d3179d68fd6da4e9284a12621ad1
svfcode <svfcode@mail.ru>
2023-11-02 15:07:01 +0300

    Upd. Added concatenate for T_CONCAT_EQUAL.

---
+++ tests/Cases/Concatenate/ConcatenateTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use CleantalkSP\Common\Scanner\HeuristicAnalyser\Controller;
+use CleantalkSP\Common\Scanner\HeuristicAnalyser\Structures\FileInfo;
+use PHPUnit\Framework\TestCase;
+
+class MathTest extends TestCase
+{
+    public function testAnalyse()
+    {
+        $current_dir = __DIR__ . DIRECTORY_SEPARATOR;
+
+        $heuristic_scanner = new Controller();
+        $file_to_check = new FileInfo('bad.php', file_get_contents($current_dir . 'bad.php'));
+        $heuristic_scanner->scanFile($file_to_check, $current_dir);
+
+        $this->assertEquals(file_get_contents($current_dir . 'good.php'), $heuristic_scanner->final_code);
+    }
+}
---
+++ tests/Cases/Math/good.php
@@ -0,0 +1,2 @@
+<?php
+ $pu8lxD9="d_sABEoe";$cJmklSv7='BAse64_dEcode';
