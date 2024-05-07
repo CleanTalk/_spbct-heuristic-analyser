02e2a0e71990c4b25aa1c3853c66c0713bdbbb38
Viktor <Bazz@bk.ru>
2024-05-07 10:26:18 +0300

    Fix. Scanner. `hex2bin` decoding fixed. (#4)

--- HeuristicAnalyser.php
+++ HeuristicAnalyser.php
@@ -365,7 +365,7 @@ class HeuristicAnalyser
 
                 // Executing decoding functions
                 // @ToDo there was many false positives!
-                // $this->transformations->decodeData($key);
+                $this->transformations->decodeData($key);
             }
 
             $this->variables->concatenate(); // Concatenates variable content if it's possible
